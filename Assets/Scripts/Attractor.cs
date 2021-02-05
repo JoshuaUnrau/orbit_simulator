e77fadd7e8e541a945167d44870a04d588ccfd8e
jpunrau <jpunrau@gmail.com>
2021-02-04 19:46:56 -0500

    Added elliptic orbits

--- Assets/Scripts/Attractor.cs
+++ Assets/Scripts/Attractor.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using DefaultNamespace;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Vector3 = UnityEngine.Vector3;
@@ -16,7 +17,7 @@ public class Attractor : MonoBehaviour
     public Shader orbitShader;
     private GameObject orbitLine;
     public Vector3 velocity;
-    
+    public EllipseRenderer renderer;
     public float radius;
     public float keneticEnergy;
     public float potenetialEnergy;
@@ -27,27 +28,40 @@ public class Attractor : MonoBehaviour
     public float orbital_period;
     public float vel_mag = 0;
     public float ang_vel = 0;
+    [Range(0,1)] public float mean_anomally;
+    [Range(0, 1)] public float ecentricity;
+    public float apoapsis;
+    public float periapsis;
+    public float semiMinor;
+    public float semiMajor;
     
     public bool moving = false;
     private int futureOrbits = 0;
-    private static int orbitOffset = 10000;
-    float timeStep = 0.1f;
-    float g = 1000;
+    public int steps = 10000;
+    public float timeStep = 0.001f;
+    public float g = 1000;
 
-    Vector3[] futurePositions = new Vector3[orbitOffset];
-    Vector3[] futureVelocities = new Vector3[orbitOffset];
+    Vector3[] futurePositions;
+    Vector3[] futureVelocities;
     
     private int counter = 60;
     // Start is called before the first frame update
     void Start()
     {
+        load();
+    }
+
+    private void load()
+    {
+        futureVelocities = new Vector3[steps];
+        futurePositions = new Vector3[steps];
         orbitParameters();
         rb.velocity += velocity;
         if (moving)
         {
             orbitLine = new GameObject();
             orbitLine.AddComponent<LineRenderer>();
-            orbitLine.GetComponent<LineRenderer>().SetVertexCount(orbitOffset);
+            orbitLine.GetComponent<LineRenderer>().SetVertexCount(steps);
             drawOrbits();
             //orbitLine.GetComponent<LineRenderer>().materials
         }
@@ -93,22 +107,27 @@ public class Attractor : MonoBehaviour
     {
         if(!moving)
             return;
-        //should be cross product
+        
         Vector3 r = orbitingBody.transform.position - transform.position;
+        radius = Vector3.Distance(orbitingBody.transform.position, transform.position);
         float mass1 = rb.mass;
         float mass2 = orbitingBody.GetComponent<Rigidbody>().mass;
         float micro = g * mass2;
+        keneticEnergy = mass1 * velocity.magnitude * velocity.magnitude / 2;
+        potenetialEnergy = g * mass2 * mass1 / r.magnitude;
         float E =  mass1 * velocity.magnitude * velocity.magnitude/2 - g * mass2 * mass1 / r.magnitude;
+        Energy = E;
         float l = Vector3.Cross(velocity, r).magnitude;
         init_energy = E;
         float a = -g*mass2*mass1/(2*E);
         float T = (float) Math.Sqrt(4 * Math.PI * Math.PI * a * a * a / (g * mass2));
-        //Below only works because r is also r_a
+
         float e = (float) Math.Sqrt(1 + 2 * E * l * l / (mass1 * mass1 * mass1 * micro * micro));
+        ecentricity = e;
 
         float theta = (float) 0;
         float per = (l * l/ (mass1*micro)) * (1 / (1 + e * (float)Math.Cos(theta)));
-        Debug.Log(E);
+        /*Debug.Log(E);
         Debug.Log(a);
         Debug.Log(e);
         Debug.Log(T);
@@ -117,8 +136,35 @@ public class Attractor : MonoBehaviour
         Debug.Log(micro);
         Debug.Log(per);
         Debug.Log((l * l/ (mass1*micro)) * (1 / (1 + e * (float)Math.Cos(Math.PI))));
-        Debug.Log((1 / (1 + e * (float)Math.Cos(theta))));
+        Debug.Log(1 / (1 + e * (float)Math.Cos(theta)));*/
+        apoapsis = (l * l / (mass1 * micro)) * (1 / (1 + e * (float) Math.Cos(Math.PI)));
+        periapsis = per;
+        //Does work if sat mass is not 1
         orbital_period = T;
+        semiMajor = (apoapsis + periapsis) / 2; 
+        semiMinor = Mathf.Sqrt(semiMajor*semiMajor*(1-e*e)); 
+        float radi = radius;
+        theta = Mathf.Acos(l * l / (radi * mass1 * mass1 * micro * e) - 1 / e);
+        Debug.Log(Mathf.Rad2Deg*theta);
+        Debug.Log(Mathf.Rad2Deg* ( Mathf.PI - theta));
+        //Control the side of the ellipse by flipping the y axis
+        //Find this by knowing if the object is on top or bottom of ellipse?
+        Vector3 p2 = new Vector3(radius*Mathf.Cos(theta),0,radius*Mathf.Sin(Mathf.PI+theta));
+        print(p2);
+        theta = Mathf.Acos((2 * radius * radius - Vector3.Distance(transform.position, p2)*Vector3.Distance(transform.position, p2)) / 
+                           (2 * radius * radius));
+        print(Mathf.Rad2Deg* theta);
+        renderer.ellipse.offsetPoint = orbitingBody.transform.position;
+        renderer.ellipse.offsetAngle = theta;
+        renderer.ellipse.xAxis = semiMajor;
+        renderer.ellipse.yAxis = semiMinor;
+        renderer.ellipse.offset.x = orbitingBody.transform.position.x-semiMajor+periapsis;
+        renderer.ellipse.offset.y = orbitingBody.transform.position.z;
+        
+        print(renderer.ellipse.offsetPoint);
+        print(renderer.ellipse.offsetAngle);
+        renderer.CalculateEllipse();
+        //What position in the orbit am I in? How do I return a future x,y cord?
     }
 
     Vector3 Attract(Vector3 position)
@@ -145,7 +191,7 @@ public class Attractor : MonoBehaviour
         //TODO: Redo with exact orbit position formula for exact values
         Vector3 startPos = rb.position;
         Vector3 startVel = velocity;
-        for(int i = 0; i < orbitOffset; i++)
+        for(int i = 0; i < steps; i++)
         {
             Vector3 accel = Attract(startPos) * timeStep;
             startVel += accel;
@@ -170,5 +216,11 @@ public class Attractor : MonoBehaviour
         lr.SetWidth(1f, 1f);
         lr.SetPosition(poolingIndex, start);
         lr.SetPosition(poolingIndex+1, end);
+    }
+
+    private void OnValidate()
+    {
+        load();
+        orbitParameters();
     }
 }
